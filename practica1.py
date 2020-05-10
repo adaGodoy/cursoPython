00b74576d3599830c4ee88535e5d3bc536450e50
Ada Godoy <ada.godoy@unah.hn>
2020-05-10 01:19:14 -0600

    primeros pasos en python

--- practica1.py
+++ practica1.py
@@ -1,2 +1,40 @@
-josue = "josue el ogro"
-print(josue)
+variable = "hola"
+variable = 2 
+#  el nombre de una variale puede ser la misma 
+# pero se ira sustituyendo a medida se cambie su valor, al final va tener el valor ultimo que se le dio
+a = 9 
+b = 8
+c = 7
+suma = a + b 
+resta = a - c
+multiplicacion = c*a
+division = b/b
+divisionEntera = b//c
+# para conocer el tipo de una variable se coloca la palabra type y dentro de parentesis la variable que se quiere conocer su tipo
+type(division)
+#tipos de mensajes
+mensaje1 = """este es un
+            un mensaje de
+            multiples lineas"""
+# se debe mantener una identacion que el programa entienda que algo pertenece a algo
+# para entender simbolos 
+# (=) es para asiganar, 
+# (==) es para comparacion
+if a > b:
+    print("el numero a es mayor que el numero b")
+else:
+    print("el numero b es mayor que el numero a")
+
+# def nombre_de_la_funcion( zona para recibir parametros): recibe distintos tipos de parametros
+#recibe instruciones de una funcion
+#return(opcional)
+
+# def nombre_de_la_funcion(parametro): aca ya tiene parametros y ya tiene una funcion establecida que recibira un tipo de parametro
+#recibe instruciones
+def mensaje ():#declaracion
+    print("Estamos aprendiendo Python")#cuerpo
+    print("estamos recibiendo instrucciones basicas")#cuerpo
+    print("poco a poco se ira avanzando")#cuerpo
+# notese que la identacion es necesaria para saber que pertenece a que
+
+mensaje()# llamando la funcion
