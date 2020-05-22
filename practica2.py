faf1be04c3e3f0796f5cddf7a689149f83544a16
Ada Godoy <ada.godoy@unah.hn>
2020-05-22 09:57:10 -0600

    practica sobre funciones

---
+++ practica2.py
@@ -0,0 +1,8 @@
+#las funciones son sistemas de los cuales se puede reutilizat su codigo de diferentes formas
+#las funciones pueden contener instrucciones especificas y tener o no parametros
+def suma(num_1, num_2):
+    print(num_1 + num_2)
+
+suma(8,9)
+suma(100000,3)
+suma(88888,1)
